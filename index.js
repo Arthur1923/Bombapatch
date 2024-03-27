b4aed1c32a290d32db15c2251a241dde635bd0f7
Arthur1923 <143663153+Arthur1923@users.noreply.github.com>
2024-03-26 22:23:05 -0300

    Carro v1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const carro = (function carro(){
    let tanque = 0 
    let velocidade = 0
+   let marcha = 0
     
     function acelerarCarro(velocidadekm) {
         return velocidade += velocidadekm;
@@ -9,27 +10,58 @@ const carro = (function carro(){
         console.log(velocidade)
     }
 
-
+    function abastecercarro (abastecercarro){
+        return tanque += abastecercarro;
+    }
+    function mostrartanque(){
+        console.log(tanque)
+    
+    }
+    
+    function mudarmarcha (mudarmarcha){
+        return marcha += mudarmarcha;
+    }
+    function mostrarmarcha(){
+        console.log('sua marcha é:' + marcha);
+    }
+    function frearcarro(frearcarro){
+       velocidade -= frearcarro;
+       console.log('freando o carro. Diminuindo ' + frearcarro + 'km')
+    }
+  
+    
     return {
         acelerar(velocidadekm) {
-            return acelerarCarro(velocidadekm);
-
+            console.log("Sua velocidade é: " + acelerarCarro(velocidadekm));
         },
         velocidade(){
-                return mostrarVelocidade();
+            return mostrarVelocidade();
         },
-        abastecer(){
-            return bombapatch(1);
+        abastecer(valorabastecido){
+            return abastecercarro(valorabastecido);
         },
-        frear(){
-            return console.log(value);
+        frear(frear){
+            frearcarro(frear);
         },
-        cambio(){
-            return console.log(value);
+        tanque(){
+            return mostrartanque();
+        },
+        mudarmarcha(){
+            mudarmarcha(1);
+            mostrarmarcha();
+        },
+        mostrarmarcha() {
+            mostrarmarcha();
         }
     }
 })();
 carro.velocidade();
-carro.acelerar(2);
-carro.velocidade();
+carro.acelerar(45);
+carro.abastecer(3);
+carro.tanque();
+carro.mudarmarcha(5);
+carro.frear(3);
+carro.acelerar(0);
+carro.mostrarmarcha();
+
 
